9fe09b1265127167c7707a919cee13413f9a6c88
j-wenning <wenning.justin@gmail.com>
2020-08-26 15:09:26 -0700

    Updated camera movement script to utilize a wider screen border for camera movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -9,6 +9,8 @@ public class CameraMovement : MonoBehaviour
     private float m_Distance = 0.05f;
     [SerializeField]
     private float m_Time = 0.1f;
+    [SerializeField, Tooltip("Distance from screen edge to activate camera movement")]
+    private int m_BorderWidth = 10;
 
     private Vector3 mNewPos;
 
@@ -21,14 +23,14 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mNewPos.y += Input.mousePosition.y >= Screen.height
+        mNewPos.y += Input.mousePosition.y >= Screen.height - m_BorderWidth
             ? m_Distance
-            : Input.mousePosition.y <= 0
+            : Input.mousePosition.y <= 0 + m_BorderWidth
             ? -m_Distance
             : 0;
-        mNewPos.x += Input.mousePosition.x >= Screen.width
+        mNewPos.x += Input.mousePosition.x >= Screen.width - m_BorderWidth
             ? m_Distance
-            : Input.mousePosition.x <= 0
+            : Input.mousePosition.x <= 0 + m_BorderWidth
             ? -m_Distance
             : 0;
         if (Input.GetButton("Jump"))
