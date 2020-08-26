8242c2380a43d39a1ff3c394ffd04bbe2ecaf33b
j-wenning <wenning.justin@gmail.com>
2020-08-26 14:47:21 -0700

    Created player movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float m_MoveSpeed;
+
+    private Vector3 mNewPos;
+
+    void Start()
+    {
+        mNewPos = transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            mNewPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mNewPos.z = transform.position.z;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, mNewPos, m_MoveSpeed);
+    }
+}
