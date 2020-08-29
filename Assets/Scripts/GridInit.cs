9223cbe85111cd58536676563fb86ecba8e2ede3
j-wenning <wenning.justin@gmail.com>
2020-08-29 01:43:48 -0700

    Created grid init script

---
+++ Assets/Scripts/GridInit.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GridInit : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_StarterChunk;
+
+    void Start()
+    {
+        // eventually set this up to read from the players last chunk theyve been on
+        Instantiate(m_StarterChunk, transform);
+    }
+}
