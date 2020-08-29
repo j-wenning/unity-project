a83197c8f23d051f93c791d2d27a689a5d7e1322
j-wenning <wenning.justin@gmail.com>
2020-08-29 14:48:49 -0700

    Created chunk loader script

---
+++ Assets/Scripts/ChunkLoader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ChunkLoader : MonoBehaviour
+{
+    private Chunk mCurChunk;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Chunk chunk))
+        {
+            chunk.LoadChunks();
+            if (mCurChunk) mCurChunk.UnloadChunks(chunk);
+            mCurChunk = chunk;
+        }
+    }
+}
