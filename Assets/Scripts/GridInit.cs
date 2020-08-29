1f455cb0ef392445cc3183f94a2d4e13182264cf
j-wenning <wenning.justin@gmail.com>
2020-08-29 14:48:16 -0700

    Updated grid init script to remove "(Clone)" from instantiated chunk

--- Assets/Scripts/GridInit.cs
+++ Assets/Scripts/GridInit.cs
@@ -5,9 +5,12 @@ public class GridInit : MonoBehaviour
     [SerializeField]
     private GameObject m_StarterChunk;
 
+    private GameObject mChunk;
+
     void Start()
     {
         // eventually set this up to read from the players last chunk theyve been on
-        Instantiate(m_StarterChunk, transform);
+        mChunk = Instantiate(m_StarterChunk, transform);
+        mChunk.name = mChunk.name.Replace("(Clone)", "");
     }
 }
