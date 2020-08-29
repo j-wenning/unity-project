ef0a8b6380c77ffe2f20e42959c6fd0aa0ac44f7
j-wenning <wenning.justin@gmail.com>
2020-08-29 01:43:30 -0700

    Created chunk script

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    public List<GameObject> m_AdjChunks = new List<GameObject>();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            LoadChunks();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            UnloadChunks();
+        }
+    }
+
+    private void LoadChunks() {
+        foreach (GameObject chunk in m_AdjChunks)
+        {
+            if (!GameObject.Find($"/Grid/{chunk.name}(Clone)"))
+            {
+                Instantiate(chunk, transform.parent);
+            }
+        }
+    }
+    
+    private void UnloadChunks() {
+        
+    }
+}
