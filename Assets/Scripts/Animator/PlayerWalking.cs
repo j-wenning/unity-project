df2297849f7397b2074d7e3a6734b14ca82e766d
j-wenning <wenning.justin@gmail.com>
2020-09-06 20:14:35 -0700

    Created player walking script

---
+++ Assets/Scripts/Animator/PlayerWalking.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class PlayerWalking : StateMachineBehaviour
+{
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (animator.GetCurrentAnimatorStateInfo(layerIndex).IsName("Dashing")) animator.SetBool("Walking", false);
+    }
+}
