007d686936ac0769b9f1b2deafbb17d888c61c79
j-wenning <wenning.justin@gmail.com>
2020-09-06 20:14:18 -0700

    Created player idle script

---
+++ Assets/Scripts/Animator/PlayerIdle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIdle : StateMachineBehaviour
+{
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool("Idling", true);
+    }
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool("Idling", false);
+    }
+}
