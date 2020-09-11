84570746141227159441dd4ee8ef20d2c5ec6ea5
j-wenning <wenning.justin@gmail.com>
2020-09-11 00:02:32 -0700

    Created player action script

---
+++ Assets/Scripts/PlayerAction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerBase m_Base;
+
+    private bool mCanBasicAttack = true;
+
+    public void TryBasicAttack()
+    {
+        if (mCanBasicAttack
+        && Input.GetButton("Fire2")
+        && m_Base.m_State.IsTag("Interruptible"))
+        {
+            StartCoroutine(BasicAttack());
+        }
+    }
+
+    private IEnumerator BasicAttack()
+    {
+        mCanBasicAttack = false;
+        yield return new WaitForEndOfFrame();
+        mCanBasicAttack = true;
+    }
+}
