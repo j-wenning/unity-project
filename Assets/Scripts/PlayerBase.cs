8466c80f31d288fe4daae1f2cad800ad8af7ba79
j-wenning <wenning.justin@gmail.com>
2020-09-11 00:02:58 -0700

    Created player base script

---
+++ Assets/Scripts/PlayerBase.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+using UnityEngine;
+
+public class PlayerBase : MonoBehaviour
+{
+    public PlayerMovement m_Movement;
+    public PlayerAction m_Action;
+    public Rigidbody2D m_Rigidbody;
+    public Collider2D m_Collider;
+    public Animator m_Animator;
+
+    [HideInInspector]
+    public AnimatorStateInfo m_State;
+    [HideInInspector]
+    public float m_DeltaTime;
+    [HideInInspector]
+    public float m_FixedTime;
+    [HideInInspector]
+    public Vector2 m_MousePos;
+
+    private void FixedUpdate()
+    {
+        m_FixedTime = Time.fixedTime;
+        m_DeltaTime = Time.fixedDeltaTime;
+        m_State = m_Animator.GetCurrentAnimatorStateInfo(m_Animator.GetLayerIndex("Base"));
+        m_MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        m_Movement.TryMoveToPos();
+        m_Movement.TryDash();
+        m_Action.TryBasicAttack();
+    }
+}
