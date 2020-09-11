b527778f3387491e188e19d1fe971c06cf4e8314
j-wenning <wenning.justin@gmail.com>
2020-09-10 23:33:00 -0700

    Updated player idle script to use updated idle param name

--- Assets/Scripts/Animator/PlayerIdle.cs
+++ Assets/Scripts/Animator/PlayerIdle.cs
@@ -1,15 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerIdle : StateMachineBehaviour
 {
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.SetBool("Idling", true);
+        animator.SetBool("Idle", true);
     }
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.SetBool("Idling", false);
+        animator.SetBool("Idle", false);
     }
 }
