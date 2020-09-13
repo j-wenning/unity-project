2429c640d484ac91d633b3487d2925ed365af91c
j-wenning <wenning.justin@gmail.com>
2020-09-13 00:28:30 -0700

    Created player state script

---
+++ Assets/Scripts/PlayerState.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public enum PlayerStateList
+{
+    Idle,
+    Walk,
+    Dash
+}
+
+public class PlayerState : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerBase m_Base;
+
+    public StateMachine Machine { get; private set; }
+
+    void Awake()
+    {
+        Machine = new StateMachine(
+            new State ("Idle", new string[] { "interruptible" }, null, new State[]
+                {
+                    new State ("Walk", new string[] { "interruptible" }, (int arg) => arg == (int)PlayerStateList.Walk),
+                    new State ("Dash", null, (int arg) => arg == (int)PlayerStateList.Dash),
+
+                }
+            ), m_Base.m_Animator);
+    }
+}
