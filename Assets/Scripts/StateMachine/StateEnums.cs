754e70de254f281e62692da88543f14880989f4f
j-wenning <wenning.justin@gmail.com>
2020-09-13 23:53:48 -0700

    Updated state enum script to include basic attack enums

--- Assets/Scripts/StateMachine/StateEnums.cs
+++ Assets/Scripts/StateMachine/StateEnums.cs
@@ -7,11 +7,21 @@
 
     public enum StateQualifier
     {
+        // Player Enums
         [Alt("Idle")]
         Player_Idle,
         [Alt("Walk")]
         Player_Walk,
         [Alt("Dash")]
-        Player_Dash
+        Player_Dash,
+        [Alt("Attack_Basic")]
+        Player_Attack_Basic,
+        [Alt("Attack_Basic_0")]
+        Player_Attack_Basic_0,
+        [Alt("Attack_Basic_1")]
+        Player_Attack_Basic_1,
+        [Alt("Attack_Basic_2")]
+        Player_Attack_Basic_2,
+        Player_Attack_Basic_End
     }
 }
