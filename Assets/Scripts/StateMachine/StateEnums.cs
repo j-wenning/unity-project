ed2319f0f600fc8685ca737b0b123c9454ee763c
j-wenning <wenning.justin@gmail.com>
2020-09-13 20:54:37 -0700

    Created state enums script

---
+++ Assets/Scripts/StateMachine/StateEnums.cs
@@ -0,0 +1,17 @@
+﻿namespace _
+{
+    public enum StateTag
+    {
+        Interruptible,
+    }
+
+    public enum StateQualifier
+    {
+        [Alt("Idle")]
+        Player_Idle,
+        [Alt("Walk")]
+        Player_Walk,
+        [Alt("Dash")]
+        Player_Dash
+    }
+}
