7890540cc2ce2dc873fc7cfb88221579fb8895dd
j-wenning <wenning.justin@gmail.com>
2020-09-14 00:41:45 -0700

    Updated player state script to include basic attack states

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -11,11 +11,19 @@ namespace _
         private State m_Idle = new State("Idle", new StateTag[] { StateTag.Interruptible }, new StateQualifier[] { StateQualifier.Player_Idle });
         private State m_Walk = new State("Walk", new StateTag[] { StateTag.Interruptible }, new StateQualifier[] { StateQualifier.Player_Walk });
         private State m_Dash = new State("Dash", null, new StateQualifier[] { StateQualifier.Player_Dash });
+        private State m_Attack_Basic = new State("Attack_Basic", new StateTag[] { StateTag.Interruptor }, new StateQualifier[] { StateQualifier.Player_Attack_Basic });
+        private State m_Attack_Basic_0 = new State("Attack_Basic_0", null, new StateQualifier[] { StateQualifier.Player_Attack_Basic_0 });
+        private State m_Attack_Basic_1 = new State("Attack_Basic_1", null, new StateQualifier[] { StateQualifier.Player_Attack_Basic_1 });
+        private State m_Attack_Basic_2 = new State("Attack_Basic_2", null, new StateQualifier[] { StateQualifier.Player_Attack_Basic_2 });
 
         public void Init()
         {
-            m_Idle.LinkNodes(m_Walk, m_Dash);
-            m_Walk.LinkNode(m_Dash);
+            m_Idle.LinkNodes(m_Walk, m_Dash, m_Attack_Basic);
+            m_Walk.LinkNodes(m_Dash, m_Attack_Basic);
+            m_Attack_Basic.LinkNodes(m_Attack_Basic_0, m_Attack_Basic_1, m_Attack_Basic_2);
+            m_Attack_Basic_0.LinkNode(m_Attack_Basic_1);
+            m_Attack_Basic_1.LinkNode(m_Attack_Basic_2);
+
             Machine = new StateMachine(m_Idle, m_Base.m_Animator);
         }
     }
